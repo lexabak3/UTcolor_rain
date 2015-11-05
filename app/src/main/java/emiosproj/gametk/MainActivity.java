a27258eaa133a88cb8ea766ebf86fe0fa3de7068
Aleks Bakshaev <lexa55212@gmail.com>
2015-11-05 13:05:20 +0600

    added new bar on playActiviti

--- app/src/main/java/emiosproj/gametk/MainActivity.java
+++ app/src/main/java/emiosproj/gametk/MainActivity.java
@@ -1,6 +1,6 @@
 package emiosproj.gametk;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity { // implements View.OnClickListener
 
     Button buttonToG;
 
@@ -19,8 +19,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.main);
 
         buttonToG = (Button) findViewById(R.id.buttonToG);
+        buttonToG.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent1 = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent1);
+            }
+        });
 
-        buttonToG.setOnClickListener(this);
+
+        //buttonToG.setOnClickListener(this);
+
     }
 
     @Override
@@ -45,10 +55,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+    /*@Override
     public void onClick(View v)
     {
         Intent intent1 = new Intent(this, PlayActivity.class);
         startActivity(intent1);
-    }
+    }*/
 }
